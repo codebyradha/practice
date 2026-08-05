91de530647da6bdca4cbeee63de8c37a1b5fc54f
codebyradha <rradhad588@gmail.com>
2026-08-05 23:49:52 +0530

    Started python

---
+++ operators.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number:"))
+
+print("Sum of",n1,"and",n2,"is:",n1+n2)
+print("Difference of",n1,"and",n2,"is:",n1-n2)
+print("Product of",n1,"and",n2,"is:",n1*n2)
+print("Quotient of",n1,"and",n2,"is:",n1/n2)
+print("Remainder of",n1,"and",n2,"is:",n1%n2)
+
+
+print("Square of ",n1,"is",n1**2)
+print("Cube of ",n2,"is",n2**3)
---
+++ personal_details.py
@@ -0,0 +1,13 @@
+name="Radha"
+college="Sheshadripuram college,Tumkur"
+course="MCA"
+city="Tumakuru"
+
+print("====Personal Details====")
+"""print("I am", name,"from", city)
+print("I am studying", course, "in", college)"""
+
+print("Name:",name)
+print("College:",college)
+print("Course:",course)
+print("City:",city)
